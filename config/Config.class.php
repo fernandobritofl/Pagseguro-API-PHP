b6ea3e1223255c46d3c9ae7065e17351e323b12b
fernandobritofl <f3rn4ndodb@gmail.com>
2012-05-27 13:23:40 -0400

    Config

---
+++ config/Config.class.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Configurações
+     * @author Fernando brito @fernandobritofl
+     */
+    class Config extends Exception
+    {
+        protected $conf = array(
+            'EMAIL'    => 'email@email.com',
+            'TOKEN'    => 'token de acesso',
+            'CURRENCY' => 'BRL' //BRL (Real)
+        );
+    }
